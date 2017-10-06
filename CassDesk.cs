8028f167a9bec2376b5c3f1e7af0276318fdda56
kihen0 <kirill.morozowhts@gmail.com>
2017-10-06 17:27:07 +0300

    First

---
+++ CassDesk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.ggg
+{
+    class CassDesk
+    {
+        double m, sigma;
+        Queue<ICustomer> queue;
+        public int QueueLength { get { return queue.Count; } }
+        public void NewCustomer(ICustomer customer)
+        {
+            queue.Enqueue(customer);
+        }
+        public ICustomer Dequeue()
+        {
+            return queue.Dequeue();
+        }
+
+
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.ggg
+{
+    static class GaussMethod
+    {
+        public static double Gauss(double m,double sigma)
+        {
+            return m * sigma;
+        }
+    }
+}
